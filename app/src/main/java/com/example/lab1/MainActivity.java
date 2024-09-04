8f5d01f76431847c67da5a406fd4cac7972f8388
Andrius <andrius2384@gmail.com>
2024-09-04 16:53:29 +0300

    Pridėtas antras mygtukas, kuris pakeičia teksto spalvą.

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.lab1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +17,17 @@ public class MainActivity extends AppCompatActivity {
         // Get references to the TextView and Button
         TextView MainText = findViewById(R.id.MainTextView);
         Button ChangeTxtBtn = findViewById(R.id.btnChangeText);
+        Button ChangeColorBtn = findViewById(R.id.btnChangeColor);
 
         // Set an OnClickListener on the Button
         ChangeTxtBtn.setOnClickListener(v -> {
             // Change the text of the TextView when the button is clicked
             MainText.setText("Tekstas Pakeistas");
+        });
+        ChangeColorBtn.setOnClickListener(v -> {
+            Random rngColor = new Random();
+            int color = Color.rgb(rngColor.nextInt(256), rngColor.nextInt(256), rngColor.nextInt(256));
+            MainText.setTextColor(color);
         });
     }
 }
