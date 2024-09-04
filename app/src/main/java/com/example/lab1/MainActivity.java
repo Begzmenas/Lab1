3bac6582b4cf0e68c9ae2ddf32172cd4d49a5a39
Andrius <andrius2384@gmail.com>
2024-09-04 16:16:17 +0300

    Pridėtas pirmojo mygtuko funkcinalumas.

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.lab1;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Get references to the TextView and Button
+        TextView MainText = findViewById(R.id.MainTextView);
+        Button ChangeTxtBtn = findViewById(R.id.btnChangeText);
+
+        // Set an OnClickListener on the Button
+        ChangeTxtBtn.setOnClickListener(v -> {
+            // Change the text of the TextView when the button is clicked
+            MainText.setText("Pakeistas tekstas");
         });
     }
 }
